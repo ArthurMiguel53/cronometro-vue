31addad7f8aa02ee5cfaa9aea46ae193b4d6f171
ArthurMiguel53 <arthurprofissional5348@gmail.com>
2023-07-07 15:57:04 -0300

    primeiro commit

---
+++ src/components/contador.vue
@@ -0,0 +1,23 @@
+<template>
+    <section>
+        <div id="contador"> 
+            Contador <span id="counter-value">1</span>
+        </div>
+    </section>
+    <aside>
+
+    </aside>
+</template>
+
+<script lang="ts">
+import { DefineComponent, defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'contador'
+});
+
+</script>
+
+<style>
+
+</style>
---
+++ src/views/MainView.vue
@@ -0,0 +1,36 @@
+<template>
+ 
+  <nav><!--menu de navegação--></nav>
+  <aside><!--conteudo secundário ou lateral--></aside>
+  <main>
+    bah meu
+    <!--conteudo principal-->
+    <!--3. utilizar o componente-->
+    <contador/>
+    <contador/>
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+// import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+//1 importar o componente
+
+import contador from '@/components/contador.vue';
+
+export default defineComponent({
+  name: 'MainView',
+  // 2. listar os componentes que vão ser utilizados
+  components: {
+    contador
+  }
+});
+
+
+
+</script>
+
+<style scoped>
+
+</style>
